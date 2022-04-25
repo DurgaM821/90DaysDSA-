a0a8b5ebe12750ed6a40cec924e6fd46648bfdf8
Durga mohan <93484022+DurgaM821@users.noreply.github.com>
2022-04-25 11:58:54 +0530

    Create patternAlphabet.java

---
+++ patternAlphabet.java
@@ -0,0 +1,37 @@
+//Alphabet pattern
+
+/* Sample input
+5
+
+sample output
+ABCDE 
+ABCDE 
+ABCDE 
+ABCDE 
+ABCDE */
+
+//PROGRAM
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int i=1;
+		while(i<=n){
+		    int j=1;
+		   
+		    while(j<=n){
+		        System.out.print((char)('A'+j-1));
+		       
+		        j=j+1;
+		    }
+		    System.out.println(" ");
+		    i=i+1;
+		}
+	}
+}
+
+
+
